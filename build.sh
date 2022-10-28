4b4409cb9470c40e61c766dacf2a83f2ea901a6b
Michael Poore <michael@michaelpoore.com>
2022-10-28 09:36:31 +0100

    Integrated docker push

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 VERSION=$(<VERSION)
 docker build . -t v12nio/codestream-ci-packer:latest -t v12nio/codestream-ci-packer:v$VERSION --build-arg VERSION=$VERSION
+docker login --username $DOCKERUSER --password $DOCKERPASS
+docker image push -a v12nio/codestream-ci-packer
