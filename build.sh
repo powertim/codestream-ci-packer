5db2eda9a86229e407f986d1f2ccc3a8947e5bf9
Michael Poore <michael@michaelpoore.com>
2022-10-28 14:34:18 +0100

    1.8.3

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 VERSION=$(<VERSION)
 docker build . -t v12nio/codestream-ci-packer:latest -t v12nio/codestream-ci-packer:v$VERSION --build-arg VERSION=$VERSION
 docker login --username $DOCKERUSER --password $DOCKERPASS
-docker image push -a v12nio/codestream-ci-packer
+docker push -a v12nio/codestream-ci-packer
