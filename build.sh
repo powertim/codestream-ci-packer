a3539595ead38a22f771c468cd627f75683e1023
Michael Poore <michael@michaelpoore.com>
2023-03-01 18:18:25 +0000

    Updated build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 VERSION=$(<VERSION)
 docker build . -t v12nio/codestream-ci-packer:latest -t v12nio/codestream-ci-packer:v$VERSION --build-arg VERSION=$VERSION
 docker login --username $DOCKERUSER --password $DOCKERPASS
-docker push -a v12nio/codestream-ci-packer
+docker push v12nio/codestream-ci-packer:latest
+docker push v12nio/codestream-ci-packer:v$VERSION
